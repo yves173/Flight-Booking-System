17be45399752ef75fe80ff443c273371bdf18308
yves173 <ykwizera67@gmail.com>
2022-04-14 15:18:06 +0200

    this system will be used as client software

---
+++ AirTicketClient/src/com/airticketserver/service/ServiceIFC.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.airticketserver.service;
+
+import com.airticketserver.model.Client;
+import com.airticketserver.model.Ticket;
+import java.rmi.*;
+import java.util.List;
+
+
+/**
+ *
+ * @author green
+ */
+public interface ServiceIFC extends Remote{
+    
+    public void saveTicket(Ticket tick) throws RemoteException;
+    
+    public List<Ticket> listTicket() throws RemoteException;
+    
+    public void saveClient(Client client)throws RemoteException;
+    
+    public List<Client> listClient() throws RemoteException;
+}
